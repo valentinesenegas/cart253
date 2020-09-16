84fe2eecb863b576f4b10436d2f6a54c8c249cb5
valentinesenegas <valentinesenegas@hotmail.com>
2020-09-16 14:51:41 -0400

    Activity 2: Started a new template project

--- activities/02-draw-an-alien/template-p5-project/js/script.js
+++ activities/02-draw-an-alien/template-p5-project/js/script.js
@@ -10,18 +10,6 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
 
-createCanvas(500, 500);
-
-background(255, 138, 1);
-
-point(250, 250);
-
-rect(0,0,150,500);
-
-line(0,0,500,500);
-
-line(500, 0, 0, 500);
-
 }
 
 // draw()
