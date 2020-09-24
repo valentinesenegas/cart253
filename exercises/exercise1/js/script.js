61f61f066a375d8a59659a1f58a3a9c7ddb5da5b
valentinesenegas <valentinesenegas@hotmail.com>
2020-09-24 11:23:22 -0400

    E1: Started exercise 1

---
+++ exercises/exercise1/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 1: I like to move it move it
+Valentine Sénégas
+
+Here is a description of this wonderful project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
