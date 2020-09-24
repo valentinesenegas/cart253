3f0d3a6691d18ba954f05c1ddb71e4cec4f7d195
valentinesenegas <valentinesenegas@hotmail.com>
2020-09-24 16:21:46 -0400

    E1: animated the background and another eye
    With mouseX and mouseY, the background changes color, and an eye changes size and color.
    Cleaned up the code and created new objects for the mouth

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -35,6 +35,7 @@ let irisEye1 = {
   x: 150,
   y:150,
   size: 40,
+  sizeRatio: 0.75,
   r: 28,
   g: 25,
   b: 20
@@ -76,6 +77,24 @@ let irisEye3 = {
   b: 20
 };
 
+let mouth = {
+  strokeColor: '#EE90C2',
+  strokeWeight: 4,
+  r:243,
+  g:178,
+  b:213,
+  arcX:256,
+  arcY:300,
+  arcWidth:80,
+  arcHeight:80,
+  arcStart:0,
+  // arcStop:PI + QUARTER_PI, CHORD
+};
+
+let tooth = {
+  arcWidth:20,
+  arcHeight:20
+};
 
 // setup()
 //
@@ -85,6 +104,7 @@ function setup() {
   noStroke();
 }
 
+
 // draw()
 //
 // Drawing of the shapes and setting the background color.
@@ -92,6 +112,14 @@ function draw() {
 
   // Background
   background(bg.r, bg.g, bg.b);
+
+  // As the mouse moves in all direction, the background color will change
+  bg.r = map(mouseY, 0, width, 108, 150);
+  bg.g = map(mouseY, 0, width, 88, 100);
+  bg.g = map(mouseX, 0, width, 188, 230);
+
+   console.log(`background.r is ${background.r}`); // NEW!
+
 
   // Head of the monster
   fill(head.r, head.g, head.b);
@@ -108,16 +136,15 @@ function draw() {
   ellipse(irisEye1.x, irisEye1.y, irisEye1.size);
 
   // As the mouse goes from left to right, the iris will go from left to right inside the eye
-  irisEye1.x = map(mouseX, 0, width, eye1.x-irisEye1.size*0.75, eye1.x+irisEye1.size*0.75);
+  irisEye1.x = map(mouseX, 0, width, eye1.x-irisEye1.size*irisEye1.sizeRatio, eye1.x+irisEye1.size*irisEye1.sizeRatio);
+
+  // As the mouse goes from the top to the bottom of the canvas, the iris will go from the top to the bottom of the eye
+  irisEye1.y = map(mouseY, 0, width, eye1.y-irisEye1.size*irisEye1.sizeRatio, eye1.y+irisEye1.size*irisEye1.sizeRatio);
 
   // The iris will stay inside the limits of the eye
-  irisEye1.x = constrain(irisEye1.x, eye1.x-irisEye1.size*0.75, eye1.x+irisEye1.size*0.75);
+  irisEye1.x = constrain(irisEye1.x, eye1.x-irisEye1.size*irisEye1.sizeRatio, eye1.x+irisEye1.size*irisEye1.sizeRatio);
+  irisEye1.y = constrain(irisEye1.y, eye1.y-irisEye1.size*irisEye1.sizeRatio, eye1.y+irisEye1.size*irisEye1.sizeRatio);
 
-  // As the mouse goes from top to bottom of the canvas, the iris will go from the top to the bottom of the eye
-  irisEye1.y = map(mouseY, 0, width, eye1.y-irisEye1.size*0.75, eye1.y+irisEye1.size*0.75);
-
-  // The iris will stay inside the limits of the eye
-  irisEye1.y = constrain(irisEye1.y, eye1.y-irisEye1.size*0.75, eye1.y+irisEye1.size*0.75);
 
 //--------- EYE 2 ---------//
 
@@ -129,7 +156,17 @@ function draw() {
   fill(irisEye2.r, irisEye2.g, irisEye2.b);
   ellipse(irisEye2.x, irisEye2.y, irisEye2.size);
 
-  //--------- EYE 3 ---------//
+  // As the mouse goes from left to right, the size of the iris will grow
+  irisEye2.size = map(mouseX, 0, width, 50, 70);
+
+  // As the mouse moves in all direction, the color of the iris will change
+  irisEye2.r = map(mouseY, 0, width, 0, 150);
+  irisEye2.g = map(mouseY, 0, width, 40, 150);
+  irisEye2.b = map(mouseX, 0, width, 60, 200);
+
+
+//--------- EYE 3 ---------//
+
   // Eye 3
   fill(eye3.r, eye3.g, eye3.b);
   ellipse(eye3.x, eye3.y, eye3.size);
@@ -138,20 +175,22 @@ function draw() {
   fill(irisEye3.r, irisEye3.g, irisEye3.b);
   ellipse(irisEye3.x, irisEye3.y, irisEye3.size);
 
-    //---- MOUTH ----//
+
+    //---- MOUTH  AND TEETH ----//
 
     // Draw the mouth
     stroke('#EE90C2');
-    strokeWeight(4);
-    fill(243, 178, 213);
-    arc(286, 340, 80, 80, 0, PI + QUARTER_PI, CHORD);
+    // stroke(arc.strokeColor);
+    strokeWeight(mouth.strokeWeight);
+    fill(mouth.r, mouth.g, mouth.b);
+    arc(mouth.arcX, mouth.arcY, mouth.arcWidth, mouth.arcHeight, mouth.arcStart, PI + QUARTER_PI, CHORD);
+
 
     // Draw the teeth
     noStroke();
     fill(1000);
-    arc(286, 330, 20, 20, 0, PI + QUARTER_PI, OPEN);
-    arc(312, 341, 20, 20, 0, PI + QUARTER_PI, OPEN);
-
+    arc(mouth.arcX, mouth.arcY-10, tooth.arcWidth, tooth.arcHeight, 0, PI + QUARTER_PI, OPEN);          // Left tooth
+    arc(mouth.arcX+26, mouth.arcY+1, tooth.arcWidth, tooth.arcHeight, 0, PI + QUARTER_PI, OPEN);       // Right tooth
 
 
 
