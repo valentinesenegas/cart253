d8ac07a626680bb810483366d73e17921d380258
valentinesenegas <valentinesenegas@hotmail.com>
2020-10-02 09:42:33 -0400

    P1: Started project 1

---
+++ projects/project1/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Project 1
+Valentine Sénégas
+
+Here is a description of this first project!
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
