91dd4f54f781166c8eaf6cd075b42d8625260f9d
valentinesenegas <valentinesenegas@hotmail.com>
2020-10-11 19:34:23 -0400

    P1: The Donalds follows the mailman and repulses the letters

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -3,7 +3,9 @@
 Project 1
 Valentine Sénégas
 
-Here is a description of this first project!
+Collect the votes before the Donald !
+Use the arrow keys to move around !
+You have one minute to collect 20 letters.
 **************************************************/
 
 let imgMailman;
@@ -26,9 +28,20 @@ let donald = {
   vx: 1,
   vy: 1,
   speed: 2,
+  attract: 300,
+  attractIntensity: 2,
+  repulsion: 500,
+  repulsionIntensity: 5
 };
 
 const mailCount = 30;
+let mailParam = {
+  initVX: 3,
+  initVY: 3,
+  maxVX: 5,
+  maxVY: 5,
+};
+
 let mails = Array();
 
 let bg = {
@@ -43,16 +56,16 @@ function createMail(imgParam) {
   let mail = {
     x: 0,
     y: 0,
-    initVX: 3,
-    initVY: 3,
-    speed: 5,
     vx: 0,
     vy: 0,
-    img: undefined
-  }
+    img: undefined,
+  };
   mail.img = imgParam;
   return mail;
 }
+
+let score = 0;
+let win = false;
 
 // preload()
 //
@@ -72,10 +85,16 @@ function setup() {
 
   // Create mails
   for (let mailIdx = 0; mailIdx < mailCount; mailIdx++) {
-    mails[mailIdx].x = random(mails[mailIdx].img.width / 2, width - mails[mailIdx].img.width / 2);
-    mails[mailIdx].y = random(mails[mailIdx].img.height / 2, height - mails[mailIdx].img.height / 2);
-    mails[mailIdx].vx = random(-mails[mailIdx].initVX, mails[mailIdx].initVX);
-    mails[mailIdx].vy = random(-mails[mailIdx].initVY, mails[mailIdx].initVY);
+    mails[mailIdx].x = random(
+      mails[mailIdx].img.width / 2,
+      width - mails[mailIdx].img.width / 2
+    );
+    mails[mailIdx].y = random(
+      mails[mailIdx].img.height / 2,
+      height - mails[mailIdx].img.height / 2
+    );
+    mails[mailIdx].vx = random(-mailParam.initVX, mailParam.initVX);
+    mails[mailIdx].vy = random(-mailParam.initVY, mailParam.initVY);
   }
 }
 
@@ -86,13 +105,12 @@ function draw() {
   background(bg.r, bg.g, bg.b);
 
   display();
-  move();      // Movement of the Donald
+  move(); // Movement of the Donald
   checkKeys(); // Movement of the mailman
   checkMailCatch();
 }
 
 function display() {
-
   // DISPLAY
   imageMode(CENTER);
   image(imgMailman, mailman.x, mailman.y);
@@ -105,26 +123,36 @@ function display() {
 function checkMailCatch() {
   for (let mailIdx = 0; mailIdx < mailCount; mailIdx++) {
     if (mails[mailIdx].img != undefined) {
-      if (mailman.x < mails[mailIdx].x + mails[mailIdx].img.width &&
-         mailman.x + imgMailman.width >mails[mailIdx].x &&
-         mailman.y < mails[mailIdx].y + mails[mailIdx].img.height &&
-         mailman.y + imgMailman.height > mails[mailIdx].y)
-          // Collision detected
-          mails[mailIdx].img = undefined;
+      if (
+        mailman.x < mails[mailIdx].x + mails[mailIdx].img.width &&
+        mailman.x + imgMailman.width > mails[mailIdx].x &&
+        mailman.y < mails[mailIdx].y + mails[mailIdx].img.height &&
+        mailman.y + imgMailman.height > mails[mailIdx].y
+      ) {
+        // Collision detected
+        mails[mailIdx].img = undefined;
+        score += 1;
+
+        if (score === mailCount) win = true;
+      }
     }
   }
 }
 
-
 function move() {
-
   // Mailman
   mailman.x = mailman.x + mailman.vx;
   mailman.y = mailman.y + mailman.vy;
 
-  // The Donald
-  donald.x = donald.x + donald.vx;
-  donald.y = donald.y + donald.vy;
+  // Donald is attracted by the mailman.
+  if (mailman.x + imgMailman.width / 2 > donald.x + imgDonald.width / 2)
+    donald.vx =  donald.attractIntensity;
+  else if (mailman.x + imgMailman.width / 2 < donald.x + imgDonald.width / 2)
+    donald.vx =  -donald.attractIntensity;
+  if (mailman.y + imgMailman.height / 2 > donald.y + imgDonald.height / 2)
+    donald.vy =  donald.attractIntensity;
+  else if (mailman.y + imgMailman.height / 2 < donald.y + imgDonald.height / 2)
+    donald.vy =  -donald.attractIntensity;
 
   // Random change of direction for the Donald
   let change = random(); // Generate a random number between 0 and 1
@@ -135,46 +163,87 @@ function move() {
     donald.vx = random(-donald.speed, donald.speed);
     donald.vy = random(-donald.speed, donald.speed);
   }
+  donald.x = donald.x + donald.vx;
+  donald.y = donald.y + donald.vy;
 
   for (let mailIdx = 0; mailIdx < mailCount; mailIdx++) {
     if (mails[mailIdx].img != undefined) {
+
+      // Check screen limits.
+/*      if (
+        mails[mailIdx].x <= mails[mailIdx].img.width / 2 ||
+        mails[mailIdx].x > width - mails[mailIdx].img.width / 2
+      )
+        mails[mailIdx].vx = -mails[mailIdx].vx;
+      if (
+        mails[mailIdx].y <= mails[mailIdx].img.height / 2 ||
+        mails[mailIdx].y > height - mails[mailIdx].img.height / 2
+      )
+        mails[mailIdx].vy = -mails[mailIdx].vy; */
+
+        if (mails[mailIdx].x <= mails[mailIdx].img.width / 2)
+          mails[mailIdx].x = width - mails[mailIdx].img.width / 2;
+        else if (mails[mailIdx].x > width - mails[mailIdx].img.width / 2)
+          mails[mailIdx].x = mails[mailIdx].img.width / 2;
+
+        if (mails[mailIdx].y <= mails[mailIdx].img.height / 2)
+          mails[mailIdx].y = height - mails[mailIdx].img.height / 2;
+        else if (mails[mailIdx].y > height - mails[mailIdx].img.height / 2)
+          mails[mailIdx].y = mails[mailIdx].img.height / 2;
+
+        let d2 = dist(mails[mailIdx].x, mails[mailIdx].y, donald.x, donald.y);
+
+        if (d2 < donald.repulsion) {
+          if (mails[mailIdx].x + mails[mailIdx].img.width / 2 > donald.x + imgDonald.width / 2)
+            mails[mailIdx].vx += ((donald.repulsion - d2) / donald.repulsionIntensity) ;
+          else if (mails[mailIdx].x + mails[mailIdx].img.width / 2 < donald.x + imgDonald.width / 2)
+            mails[mailIdx].vx -= ((donald.repulsion - d2) / donald.repulsionIntensity)  ;
+          if (mails[mailIdx].y + mails[mailIdx].img.height / 2 > donald.y + imgDonald.height / 2)
+            mails[mailIdx].vy += ((donald.repulsion - d2) / donald.repulsionIntensity) ;
+          else if (mails[mailIdx].y + mails[mailIdx].img.height / 2 < donald.y + imgDonald.height / 2)
+            mails[mailIdx].vy -= ((donald.repulsion - d2) / donald.repulsionIntensity) ;
+        }
+
+      // Be sure not to overspeed (because we don't like tickets).
+      if (mails[mailIdx].vx > 0 && mails[mailIdx].vx > mailParam.maxVX)
+        mails[mailIdx].vx = mailParam.maxVX;
+      else if (mails[mailIdx].vx < 0 && mails[mailIdx].vx < -mailParam.maxVX)
+        mails[mailIdx].vx = -mailParam.maxVX;
+      if (mails[mailIdx].vy > 0 && mails[mailIdx].vy > mailParam.maxVY)
+        mails[mailIdx].vy = mailParam.maxVY;
+      else if (mails[mailIdx].vy < 0 && mails[mailIdx].vy < -mailParam.maxVY)
+        mails[mailIdx].vy = -mailParam.maxVY;
+
       mails[mailIdx].x += mails[mailIdx].vx;
       mails[mailIdx].y += mails[mailIdx].vy;
-      if (mails[mailIdx].x <= mails[mailIdx].img.width / 2 || mails[mailIdx].x > width - mails[mailIdx].img.width / 2)
-        mails[mailIdx].vx = -mails[mailIdx].vx;
-      if (mails[mailIdx].y <= mails[mailIdx].img.height / 2 || mails[mailIdx].y > height - mails[mailIdx].img.height / 2)
-        mails[mailIdx].vy = -mails[mailIdx].vy;
     }
   }
 }
-
 
 function checkKeys() {
   // Allows the user to move the mailman using the arrow keys!
 
   if (keyIsDown(LEFT_ARROW)) {
-  // If it is, set the x velocity to be negative
-  mailman.vx = -mailman.speed;
+    // If it is, set the x velocity to be negative
+    mailman.vx = -mailman.speed;
   }
   // Otherwise is the right arrow pressed?
   else if (keyIsDown(RIGHT_ARROW)) {
-  // If it is, set the x velocity to be positive
-  mailman.vx = mailman.speed;
+    // If it is, set the x velocity to be positive
+    mailman.vx = mailman.speed;
   }
   // If neither of those keys are pressed
   else {
-  // Then set the x velocity to 0 to stop moving horizontally
-  mailman.vx = 0;
+    // Then set the x velocity to 0 to stop moving horizontally
+    mailman.vx = 0;
   }
 
   // Do the same thing with vertical movement and the UP and DOWN keys
   if (keyIsDown(UP_ARROW)) {
-  mailman.vy = -mailman.speed;
-  }
-  else if (keyIsDown(DOWN_ARROW)) {
-  mailman.vy = mailman.speed;
-  }
-  else {
-  mailman.vy = 0;
+    mailman.vy = -mailman.speed;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    mailman.vy = mailman.speed;
+  } else {
+    mailman.vy = 0;
   }
 }
