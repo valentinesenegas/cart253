4be698cf8a84f2b60cd8241ef11740d792ed66c7
valentinesenegas <valentinesenegas@hotmail.com>
2020-10-22 16:26:05 -0400

    E4: Prepared images, added a user-controlled ghost

--- exercises/exercise4/README.md
+++ exercises/exercise4/README.md
@@ -1,3 +1,7 @@
 # Template p5 project
 
 This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+
+Attribution:
+Candy illustrations: Design vector created by freepik - www.freepik.com
+Ghost illustration: Party vector created by pikisuperstar - www.freepik.com
--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -6,28 +6,47 @@ Valentine Sénégas
 Here is a description of this template p5 project.
 **************************************************/
 
-// Our fish
+// Our candies
 let school = [];
 let schoolSize = 4;
+
+let imgGhost;
+
+let ghost = {
+  x: 70,
+  y: 70,
+  vx: 1,
+  vy: 1,
+  speed: 3
+}
+
+// preload()
+//
+function preload() {
+  imgGhost = loadImage('assets/images/ghost.png');
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 800);
 
-  // Create four fish, positioned randomly
-  // Create four fish, positioned randomly, storing each one in four successive
+  // Create four candy, positioned randomly
+  // Create four candy, positioned randomly, storing each one in four successive
   // spaces in our array by using the addresses `0` through `3`
   for (let i = 0; i < schoolSize; i++) {
-    school[i] = createFish(random(0, width), random(0, height));
+    school[i] = createCandy(random(0, width), random(0, height));
   }
+
+  ghost.x = width / 3;
+  ghost.y = height / 2;
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
+// createCandy(x,y)
+// Creates a new JavaScript Object describing a candy and returns it
+function createCandy(x, y) {
+  let candy = {
     x: x,
     y: y,
     size: 50,
@@ -35,47 +54,94 @@ function createFish(x, y) {
     vy: 0,
     speed: 2
   };
-  return fish;
+  return candy;
 }
 
 // draw()
-// Moves and displays our fish
+// Moves and displays our candy
 function draw() {
   background(0);
 
   // Use a for loop to count from 0 up to 3
-  // and move the fish at that index in the schools array each time and display the fish
+  // and move the candy at that index in the schools array each time and display the candy
   for (let i = 0; i < 4; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
+    moveCandy(school[i]);
+    displayCandy(school[i]);
   }
+
+    displayUser();
+    moveUser();
 }
 
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
+// moveCandy(candy)
+// Chooses whether the provided candy changes direction and moves it
+function moveCandy(candy) {
   // Choose whether to change direction
   let change = random(0, 1);
   if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+    candy.vx = random(-candy.speed, candy.speed);
+    candy.vy = random(-candy.speed, candy.speed);
   }
 
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
+  // Move the candy
+  candy.x = candy.x + candy.vx;
+  candy.y = candy.y + candy.vy;
 
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
+  // Constrain the candy to the canvas
+  candy.x = constrain(candy.x, 0, width);
+  candy.y = constrain(candy.y, 0, height);
 }
 
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
+// displayCandy(candy)
+// Displays the provided candy on the canvas
+function displayCandy(candy) {
   push();
   fill(200, 100, 100);
   noStroke();
-  ellipse(fish.x, fish.y, fish.size);
+  ellipse(candy.x, candy.y, candy.size);
   pop();
+}
+
+function displayUser() {
+  imageMode(CENTER);
+  image(imgGhost, ghost.x, ghost.y);
+
+  // Constrain the ghost to the canvas
+  ghost.x = constrain(ghost.x, 0, width);
+  ghost.y = constrain(ghost.y, 0, height);
+}
+
+
+function moveUser() {
+
+  ghost.x = ghost.x + ghost.vx;
+  ghost.y = ghost.y + ghost.vy;
+
+  // Allows the user to move the ghost using the arrow keys!
+
+  if (keyIsDown(LEFT_ARROW)) {
+  // If it is, set the x velocity to be negative
+  ghost.vx = -ghost.speed;
+  }
+  // Otherwise is the right arrow pressed?
+  else if (keyIsDown(RIGHT_ARROW)) {
+  // If it is, set the x velocity to be positive
+  ghost.vx = ghost.speed;
+  }
+  // If neither of those keys are pressed
+  else {
+  // Then set the x velocity to 0 to stop moving horizontally
+  ghost.vx = 0;
+  }
+
+  // Do the same thing with vertical movement and the UP and DOWN keys
+  if (keyIsDown(UP_ARROW)) {
+  ghost.vy = -ghost.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+  ghost.vy = ghost.speed;
+  }
+  else {
+  ghost.vy = 0;
+  }
 }
