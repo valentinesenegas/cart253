7938b74e0f6bf7ebf0d9a09e2e0de84191d0cca4
valentinesenegas <valentinesenegas@hotmail.com>
2020-11-04 15:30:34 -0500

    P2: Started prototype for project 2

---
+++ projects/project2/prototype/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Prototype for Project 2
+Valentine Sénégas
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
