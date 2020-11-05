708738bd9f741c93dd64443b5f1704494725934f
valentinesenegas <valentinesenegas@hotmail.com>
2020-11-05 10:43:51 -0500

    A8: Started the Inheritance activity

---
+++ activities/08-inheritance-activity/js/script.js
@@ -0,0 +1,19 @@
+/**************************************************
+Inheritance Activity
+Valentine Sénégas
+
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
