1e2779a863627a3982dd3c2dd8bb6ffd16ca56e4
valentinesenegas <valentinesenegas@hotmail.com>
2020-11-12 14:06:31 -0500

    Activity: Started the sound activity

---
+++ activities/sound-activity/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Sound Activity
+Valentine Sénégas
+
+Here is a description of this Sound Activity.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
