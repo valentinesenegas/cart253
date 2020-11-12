bdec3a8f813091d3ce4bfc8188893291c92b89bc
valentinesenegas <valentinesenegas@hotmail.com>
2020-11-12 15:03:07 -0500

    Activity: Completed the sound activity

--- activities/sound-activity/js/script.js
+++ activities/sound-activity/js/script.js
@@ -2,19 +2,44 @@
 Sound Activity
 Valentine Sénégas
 
-Here is a description of this Sound Activity.
+A program that plays music based on primitive physics
 **************************************************/
+
+// The balls
+let balls = [];
+
+// F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
 // setup()
 //
-// Description of setup() goes here.
+// Just creates the canvas
 function setup() {
+  createCanvas(600, 600);
 
+  userStartAudio();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+  let note = random(notes);
+  let ball = new Ball(x, y, note);
+  balls.push(ball);
 }
